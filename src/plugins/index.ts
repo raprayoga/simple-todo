5f789903a73fbadaf21bd326f0f9dcf7bb2db7f0
raprayoga <prayogarafie@gmail.com>
2023-11-02 16:32:57 +0700

    fix(vercel): fix vercel 404

--- src/plugins/index.ts
+++ src/plugins/index.ts
@@ -2,8 +2,12 @@ import router from '@/router'
 import { createPinia } from 'pinia'
 
 import type { App } from 'vue'
+import type { RouteLocationRaw } from 'vue-router'
 
 export function registerPlugins(app: App) {
   app.use(createPinia())
   app.use(router)
+  app.config.globalProperties.$redirect = (page: RouteLocationRaw) => {
+    router.push(page)
+  }
 }
